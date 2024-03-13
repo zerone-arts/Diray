2e171d058340e6229c7afba7edc5fedfb99594cb
zerone-arts <zerone-@naver.com>
2024-03-13 16:29:06 +0900

    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
